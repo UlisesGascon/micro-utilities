6bb8ba61654d8137ad6c37103a2864da9e8b9ad9
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-08-02 09:25:38 +0200

    test: added test for text-to-array

---
+++ packages/string-to-array/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import { stringToArray } from '../src'
+
+describe('string-to-array', () => {
+    it('Should convert an array to string', () => {
+      expect(stringToArray("hello,world ")).toStrictEqual(['hello', 'world'])
+    })
+
+    it('Should support custom separators', () => {
+      expect(stringToArray("hello+world", "+")).toStrictEqual(['hello', 'world'])
+    })
+
+    it('Should remove empty values', () => {
+      expect(stringToArray("John, Lisa, , , James")).toStrictEqual(['John', 'Lisa', 'James'])
+    })
+
+    it('Should ignore blank spaces', () => {
+      expect(stringToArray("   John, Lisa ,  James  ")).toStrictEqual(['John', 'Lisa', 'James'])
+    })
+
+    it('Should always return an array', () => {
+      expect(stringToArray("")).toStrictEqual([])
+    })
+})
